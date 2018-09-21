6d0928294b7b32e1d8fa3de8a7cdd2c2f1bbfa75
William Kulha <kulha.william@gmail.com>
2018-09-21 11:32:31 -0400

    Add javascript that makes the page function as it should.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,23 @@
 $(document).ready(function(){
   $('#userInput').submit(function(event) {
     event.preventDefault();
-    var addWord = $('#userSentence').val();
-    console.log(addWord);
+    //Take the user's input and save it as a variable.
+    var addWords = $('#userSentence').val();
+    //remove punctuation from the end of the string.
+    var noPunctuation = addWords.replace(/[!.,?]/g, "");
+    //split the original string into array indices
+    var initialSentence = noPunctuation.split(" ");
+    var greaterThanThree = []
+    //search for words that are three characters or longer
+    initialSentence.forEach(function(word) {
+      if (word.length >= 3) {
+        greaterThanThree.push(word);
+      }
+    });
+    //reverse the array and then split it back into a string
+    wordPlay = greaterThanThree.reverse().join(" ");
+    $('#wordPlay').text(wordPlay);
+
+    console.log(initialSentence);
   })
 })
